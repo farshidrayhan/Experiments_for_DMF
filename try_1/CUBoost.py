9f3977f497a19ad0c6bab2adfd0c1b955df37c9a
farshid <farshidrayhan57@gmail.com>
2018-01-03 03:11:51 +0600

    Kaggle stuff

--- try_1/CUBoost.py
+++ try_1/CUBoost.py
@@ -11,6 +11,7 @@ from imblearn.under_sampling import AllKNN
 from imblearn.under_sampling import ClusterCentroids
 from imblearn.under_sampling import EditedNearestNeighbours
 from sklearn.cluster import KMeans
+from sklearn.ensemble.weight_boosting import _samme_proba
 from sklearn.tree import DecisionTreeClassifier
 from imblearn.under_sampling import RandomUnderSampler
 from imblearn.over_sampling import SMOTE
@@ -20,9 +21,10 @@ class AdaBoost:
     def __init__(self, M, depth,neighbours):
         self.M = M
         self.depth = depth
-        # self.undersampler = RandomUnderSampler(return_indices=True,replacement=False)
-        self.undersampler = EditedNearestNeighbours(return_indices=True,n_neighbors=neighbours)
+        self.undersampler = RandomUnderSampler(return_indices=True,replacement=False)
 
+        ## Some other samplers to play with ######
+        # self.undersampler = EditedNearestNeighbours(return_indices=True,n_neighbors=neighbours)
         # self.undersampler = AllKNN(return_indices=True,n_neighbors=neighbours,n_jobs=4)
 
     def fit(self, X, Y):
@@ -36,22 +38,6 @@ class AdaBoost:
             tree = DecisionTreeClassifier(max_depth=self.depth, splitter='best')
 
             X_undersampled, y_undersampled, chosen_indices = self.undersampler.fit_sample(X, Y)
-
-            # this part for SMOTE
-            # ==============================================================================
-            #       len_before_sampling = len(y_undersampled)
-            #
-            #       X_undersampled , y_undersampled = self.oversampler.fit_sample(X,Y)
-            #
-            #       len_after_sampling = len(y_undersampled)
-            #
-            #       data_increased = len_after_sampling - len_before_sampling
-            #
-            #       new_weights = np.ones(data_increased) / data_increased
-            #
-            #       weights = np.concatenate((W[chosen_indices],new_weights),axis=0)
-            # ==============================================================================
-
             tree.fit(X_undersampled, y_undersampled, sample_weight=W[chosen_indices])
 
             P = tree.predict(X)
@@ -75,8 +61,6 @@ class AdaBoost:
                     # W = W * np.exp(-alpha * Y * P)  # vectorized form
                     W = W / W.sum()  # normalize so it sums to 1
 
-
-
                 self.models.append(tree)
                 self.alphas.append(alpha)
 
@@ -86,3 +70,41 @@ class AdaBoost:
         for alpha, tree in zip(self.alphas, self.models):
             FX += alpha * tree.predict(X)
         return np.sign(FX), FX
+
+    def predict_proba(self, X):
+        # if self.alphas == 'SAMME'
+        proba = sum(tree.predict_proba(X) * alpha for tree , alpha in zip(self.models,self.alphas) )
+
+
+        proba = np.array(proba)
+
+
+        proba = proba / sum(self.alphas)
+
+        proba = np.exp((1. / (2 - 1)) * proba)
+        normalizer = proba.sum(axis=1)[:, np.newaxis]
+        normalizer[normalizer == 0.0] = 1.0
+        # proba =  np.linspace(proba)
+        # proba = np.array(proba).astype(float)
+        proba = proba /  normalizer
+
+        # print(proba)
+        return proba
+
+    def predict_proba_samme(self, X):
+        # if self.alphas == 'SAMME.R'
+        proba = sum(_samme_proba(est , 2 ,X) for est in self.models )
+
+        proba = np.array(proba)
+
+        proba = proba / sum(self.alphas)
+
+        proba = np.exp((1. / (2 - 1)) * proba)
+        normalizer = proba.sum(axis=1)[:, np.newaxis]
+        normalizer[normalizer == 0.0] = 1.0
+        # proba =  np.linspace(proba)
+        # proba = np.array(proba).astype(float)
+        proba = proba / normalizer
+
+        # print('proba = ',proba)
+        return proba.astype(float)
